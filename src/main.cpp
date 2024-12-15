a3f3b9400e00975c2d27b49589b504034daecff5
kf-c <capybara.captain@qq.com>
2024-12-15 23:09:15 +0800

    基于定时中断

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,50 @@
-//#include <TFT_eSPI.h>
+#define TFT
+#include <TFT_eSPI.h>
 #include<WiFi.h>
 #include <HTTPClient.h>
 #include<time.h>
-//TFT_eSPI tft = TFT_eSPI();
+#include<fuc.h>
+TFT_eSPI tft = TFT_eSPI();
 const char* ssid = "m0nesy";
 const char* password = "by060326";
 // 网络时间服务器地址，这里以NTP服务器为例
 const char* ntpServer = "pool.ntp.org";
 const long utcOffsetInSeconds = 28800;
-
+hw_timer_t *timer = NULL;
 void setup(){
- /*     tft.init();//初始化
-      tft.setRotation(0);                 //屏幕旋转方向
-      tft.fillScreen(TFT_BLACK);//底色
-      tft.setTextFont(2);
-      tft.setTextSize(2);                 //字体大小
-      tft.setTextColor(TFT_DARKGREEN);    //文字颜色
-      // sprite.setSwapBytes(true);//决定字体是RGB还是BGR
-      tft.setCursor(10,10);
-*/               //文字位置
-      //WiFi 连接
-      Serial.begin(115200);
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-      }
-      Serial.println("connected. ");//打印汉字
-      //time
-configTime(utcOffsetInSeconds, 0, ntpServer);
-  while (!time(nullptr)) {
-    delay(1000);
-    Serial.println("Waiting for time sync...");
+  tft.init();//初始化
+  tft.setRotation(3);                 //屏幕旋转方向
+  tft.fillScreen(TFT_BLACK);//底色
+  tft.setTextFont(2);
+  tft.setTextSize(2);                 //字体大小
+  tft.setTextColor(TFT_DARKGREEN);    //文字颜色
+  // sprite.setSwapBytes(true);//决定字体是RGB还是BGR
+  tft.setCursor(10,10);               //设置光标位置
+  //WiFi 连接
+  int try_time=-30;
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED&&try_time) {
+  delay(500);try_time++;
+  tft.print(".");
   }
-  Serial.println("Time synced successfully");
+  if(try_time){
+    print(tft,"Connection Success");
+  }
+  else{
+    print(tft,"Connection Failed");
+  }
+  //time
+  configTime(utcOffsetInSeconds, 0, ntpServer);
+  while (!time(nullptr)) {
+    print(tft,"Waiting for time sync...");
+  }
+  print(tft,"Waiting for time sync...");
+  //定时器
+  timer = timerBegin(0, 80, true); // 使用定时器 0，预分频器 80，计数器向上
+  timerAttachInterrupt(timer, &onTimer, true); // 附加中断服务例程，启用真正的硬件中断
+  timerAlarmWrite(timer, 10000, true); // 设置定时器为 1 秒（1000000 微秒）
+  timerAlarmEnable(timer); // 启用定时器中断
+}
+void loop(){
 }
 
-void loop() {
-  time_t now = time(nullptr);
-  Serial.print("Current time is: ");
-  Serial.println(ctime(&now));
-  delay(1000);
-  Serial.println("");
-  //if you can see the time, the program is working
-}
