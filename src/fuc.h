e19f82f8f58d2a9dce8adccef85d826980d614fb
kf-c <capybara.captain@qq.com>
2024-12-17 22:41:50 +0800

    time.Serial.print

--- src/fuc.h
+++ src/fuc.h
@@ -1,26 +1,44 @@
-#ifndef TFT
-#define TFT
-#include <TFT_eSPI.h>
-//#include"main.cpp"
-#endif
-extern TFT_eSPI tft;
+
+extern WiFiUDP ntpUDP;
+extern NTPClient timeClient;
+
 void print(
-    TFT_eSPI& tft,
      const char* str1,
      const char* str2 = nullptr,
      const char* str3 = nullptr
 ) {
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString(str1,10,10);
+    Serial.print(str1);
     if (str2 != nullptr) {
-        tft.drawString(str2, 10, 20 + tft.fontHeight(2));
+        Serial.print(str2);
     }
 
     if (str3 != nullptr) {
-        tft.drawString(str3, 10, 30 + tft.fontHeight(2) * 2);
+        Serial.print(str3);
     }
     delay(1000); // 示例：在屏幕左上角绘制字符串
 }
-void onTimer() {
-    print(tft,"time:",ctime((const time_t*)time(nullptr)));
-}//Serial.print
+
+
+void Display_Time() {
+  timeClient.update();
+  unsigned long epochTime = timeClient.getEpochTime();
+  //打印时间
+  int currentSec = timeClient.getSeconds();
+  int currentMinute = timeClient.getMinutes();
+  int currentHour = timeClient.getHours();
+  int weekDay = timeClient.getDay();
+  //将epochTime换算成年月日
+  struct tm *ptm = gmtime ((time_t *)&epochTime);
+  int monthDay = ptm->tm_mday;
+  int currentMonth = ptm->tm_mon+1;
+  int currentYear = ptm->tm_year+1900;
+  if(currentSec<10){
+    Serial.print("0");
+    Serial.print(currentSec);
+  }
+  else Serial.print(currentSec);
+  Serial.print(":");
+  Serial.print(currentMinute);
+  Serial.print(":");
+  Serial.print(currentHour);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,37 @@
-//#include <TFT_eSPI.h>
-#include<WiFi.h>
+#define MAIN
+#include <Arduino.h>
+#include <WiFi.h>
 #include <WiFiUdp.h>
 #include <HTTPClient.h>
-#include <NTPClient.h>//时间服务提供程序
-//TFT_eSPI tft = TFT_eSPI();
+#include <NTPClient.h> // 时间服务提供程序
+#include "fuc.h"
+
 const char* ssid = "m0nesy";
 const char* password = "by060326";
 // 网络时间服务器地址，这里以NTP服务器为例
 const char* ntpServer = "pool.ntp.org";
 const long utcOffsetInSeconds = 28800;
-
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP,"ntp.aliyun.com");
-
+NTPClient timeClient(ntpUDP, "ntp.aliyun.com");
 void setup(){
-
-      //WiFi 连接
-      Serial.begin(115200);
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-      }
-      Serial.println("connected. ");//打印汉字
-      //time
-configTime(utcOffsetInSeconds, 0, ntpServer);
-  while (!time(nullptr)) {
-    delay(1000);
-    Serial.println("Waiting for time sync...");
-  }
-  Serial.println("Time synced successfully");
+    // WiFi 连接
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("connected. "); // 打印汉字
+    // time
+    timeClient.begin();
+    timeClient.setTimeOffset(28800);  // + 1区 偏移3600， +8区 ：3600×8 = 2880
 }
 
+int lastTimer = millis();
+
 void loop() {
-  time_t now = time(nullptr);
-  Serial.print("Current time is: ");
-  Serial.println(ctime(&now));
-  delay(1000);
-  Serial.println("");
-  //if you can see the time, the program is working
+    if (millis() - lastTimer > 1000) {
+        lastTimer = millis();
+        Display_Time();
+    }
 }
