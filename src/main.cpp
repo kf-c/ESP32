f61e948f52822e79a5f699d09693283599e4f405
kf-c <capybara.captain@qq.com>
2024-12-20 00:54:15 +0800

    weather_fuc_done

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
 // 替换为你自己的WiFi网络名称（SSID）和密码
-const char* ssid = "m0nesy";
-const char* password = "by060326";
+//const char* ssid = "m0nesy";
+//const char* password = "by060326";
+const char* ssid = "ppq";
+const char* password = "pppppppp";
 // 替换为你申请到的心知天气API Key
 const char* apiKey = "S1Ahf1JET_NF5BU4b";
 // 心知天气API的基础URL
-const char* apiUrl = "api.seniverse.com";
 const char* host = "api.seniverse.com";
+const char* apiUrl = "https://api.seniverse.com/v3/weather/now.json";
 // 要查询天气的城市名称，可按需修改
 const char* city = "xiamen";
 WiFiClient client;
@@ -40,37 +42,32 @@ void loop() {
     delay(1000);
 }          
 bool getWeatherInfo() {
-    if (client.connect(apiUrl, 80)) {
+    if (client.connect(host, 80)) {
         Serial.println("已连接到天气服务器");
-        // 构建请求URL，按照心知天气API要求的格式
-        /*String requestUrl = "/v3/weather/now.json?key=" + String(apiKey) + "=" + String(city) + "&language=zh-Hans&unit=c";
-        Serial.println("正在发送请求: " + requestUrl);
-        client.print(String("GET ") + requestUrl + " HTTP/1.1\r\n" +
-             "Host: " + apiUrl + "\r\n" +
-             "Connection: close\r\n\r\n");*/
-
-
-//https://api.seniverse.com/v3/weather/now.json?key=SIMHXoFn5ZYDmEql3&location=xiamen&language=zh-Hans&unit=c
-        //URL请求地址 //改为你的api密钥和城市拼音
-        String url ="/v3/weather/now.json?key=SIMHXoFn5ZYDmEql3&location=xiamen&language=zh-Hans&unit=c";
-        //发送网络请求
-        client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                    "Host: " + host + "\r\n" +
-                    "Connection: close\r\n\r\n");
-        //delay(5000);
+         // 构建请求URL
+        String requestUrl = String(apiUrl) + "?key=" + apiKey + "&location=" + city + "&language=zh-Hans&unit=c";
+        // 发送GET请求
+        client.print("GET ");
+        client.print(requestUrl);
+        client.print(" HTTP/1.1\r\n");
+        client.print("Host: ");
+        client.print(host);         //url or host
+        client.print("\r\n");
+        client.print("Connection: close\r\n");
+        client.print("\r\n");
+        delay(4000);
+        Serial.printf("Initial available bytes: %d\n", client.available());
+        // 接收服务器响应
         //定义answer变量用来存放请求网络服务器后返回的数据
         String answer;
-        Serial.printf("Initial available bytes: %d\n", client.available());
         while(client.available())
         {
-            //String line = client.readStringUntil('\r');
-            //answer += line;
-            answer += (char)client.read();
+            String line = client.readStringUntil('\r');
+            answer += line;
         }
         //断开服务器连接
         client.stop();
-        //Serial.println();
-        Serial.println(answer+url);
+        Serial.println("answer:"+answer);
         Serial.println("closing connection");
 
         //获得json格式的数据
@@ -95,15 +92,19 @@ bool getWeatherInfo() {
             return 0;
 
         }
+        else Serial.println("deserializeJson() success");
         // 提取天气信息
         const char* weather = doc["results"][0]["now"]["text"].as<const char*>();
+        const char* location = doc["results"][0]["location"]["name"].as<const char*>();
         double temperature = doc["results"][0]["now"]["temperature"].as<double>();
-        Serial.println("当前城市: " + String(city));
+        Serial.println("当前城市: " + String(location));
         Serial.println("天气状况: " + String(weather));
         Serial.println("当前温度: " + String(temperature) + "℃");
+        
         return true;
     } else {
         Serial.println("无法连接到天气服务器");
         return false;
     }
+    
 }
