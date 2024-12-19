69a2e1ab7bfc349d20ba7f578c15ad3b154037f6
kf-c <capybara.captain@qq.com>
2024-12-19 17:39:57 +0800

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,55 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
+#include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-// 替换为你自己的WiFi网络名称（SSID）和密码
-const char* ssid = "ppq";
-const char* password = "pppppppp";
+const char *ssid = "XMUNET+";
+const char *password = "Ckf84861306";
+String url = "https://api.seniverse.com/v3/weather/now.json";
+String city = "厦门";
+String key = "S1Ahf1JET_NF5BU4b";
 
-// 替换为你申请到的心知天气API Key
-const char* apiKey = "S1Ahf1JET_NF5BU4b";
-// 心知天气API的基础URL
-const char* apiUrl = "https://api.seniverse.com/v3/weather/now.json";
-// 要查询天气的城市名称，可按需修改
-const char* city = "Xiamen";
-
-WiFiClient client;
-
-// 记录上次更新天气信息的时间戳（毫秒），用于定时更新
-unsigned long lastUpdateTime = 0;
-// 设置更新间隔，这里设为10分钟（600000毫秒），可根据需求调整
-const unsigned long updateInterval = 3000; 
-bool getWeatherInfo();
 void setup() {
-    Serial.begin(115200);
-    // 连接WiFi网络
-    WiFi.begin(ssid, password);
-    while (WiFi.status()!= WL_CONNECTED) {
-        delay(1000);
-        Serial.println("正在连接WiFi...");
-    }
-    Serial.println("已连接到WiFi网络");
+  Serial.begin(11520);
+  while(!Serial);
+  Serial.println("Booting");
+  // 连接WiFi
+  /*WiFi.begin(ssid, password);
+  while (WiFi.status()!= WL_CONNECTED) {
+    delay(1000);
+    Serial.println("WAiting...");
+  }
+  Serial.println("连接成功");
+  Serial.print("IP地址:");
+  Serial.println(WiFi.localIP());*/
 }
 
 void loop() {
-    if (millis() - lastUpdateTime >= updateInterval) {
-        if (getWeatherInfo()) {
-            Serial.println("天气信息更新成功");
-        } else {
-            Serial.println("天气信息更新失败，请检查网络或API设置");
-        }
-        lastUpdateTime = millis();
-    }
-    delay(1000);
-}
-
-bool getWeatherInfo() {
-    if (client.connect(apiUrl, 80)) {
-        // 构建请求URL，按照心知天气API要求的格式
-        String requestUrl = "/v3/weather/now.json?key=" + String(apiKey) + "&location=" + String(city) + "&language=zh-Hans&unit=c";
-        Serial.println("正在发送请求: " + requestUrl);
-        client.println("GET " + requestUrl + " HTTP/1.1");
-        client.println("Host: api.seniverse.com");
-        client.println("Connection: close");
-        client.println();
-
-        // 等待服务器响应
-        while (client.connected() &&!client.available()) {
-            delay(100);
-        }
-
-        // 读取响应数据
-        String response = "";
-        while (client.available()) {
-            response += (char)client.read();
-        }
-        client.stop();
-
-        // 使用ArduinoJson库解析JSON数据
-        StaticJsonDocument<2048> doc;  // 根据实际返回数据大小适当调整容量
-        DeserializationError error = deserializeJson(doc, response);
-        if (error) {
-            Serial.println("解析JSON数据出错: " + String(error.c_str()));
-            return false;
-        }
-
-        // 提取天气信息
-        const char* weather = doc["results"][0]["now"]["text"].as<const char*>();
-        double temperature = doc["results"][0]["now"]["temperature"].as<double>();
-        Serial.println("当前城市: " + String(city));
-        Serial.println("天气状况: " + String(weather));
-        Serial.println("当前温度: " + String(temperature) + "℃");
-        return true;
-    } else {
-        Serial.println("无法连接到天气服务器");
-        return false;
-    }
+  /*// 创建HTTPClient对象
+  HTTPClient http;
+  // 发送GET请求
+  http.begin(url + "?key=" + key + "&location=" + city);
+  int httpCode = http.GET();
+  // 获取响应状态码
+  Serial.printf("HTTP状态码:%d\n", httpCode);
+  if (httpCode == HTTP_CODE_OK) {
+    // 获取响应正文
+    String response = http.getString();
+    Serial.println("响应数据");
+    Serial.println(response);
+    // 创建DynamicJsonDocument对象
+    DynamicJsonDocument doc(1024);
+    // 解析JSON数据
+    deserializeJson(doc, response);
+    // 从解析后的JSON文档中获取值
+    int temp = doc["results"][0]["now"]["temperature"].as<int>();
+    String name = doc["results"][0]["location"]["name"].as<String>();
+    String wea = doc["results"][0]["now"]["text"].as<String>();
+    String Time = doc["results"][0]["last_update"].as<String>();
+    Serial.printf("位置：%s\n", name);
+    Serial.printf("温度：%d\n", temp);
+    Serial.printf("天气：%s\n", wea);
+    Serial.printf("更新时间：%s\n", Time);
+  }
+  http.end();
+  delay(6000);*/
 }
