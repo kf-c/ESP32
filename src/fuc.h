2ff92d4992b646df634770c013ba3993ecfb2715
kf-c <capybara.captain@qq.com>
2024-12-20 10:43:58 +0800

    ..

--- src/fuc.h
+++ src/fuc.h
@@ -1,7 +1,16 @@
 #include <ArduinoJson.h>
+#include <WiFiClient.h>
+// 替换为你申请到的心知天气API Key
+const char* apiKey = "S1Ahf1JET_NF5BU4b";
+// 心知天气API的基础URL
+const char* host = "api.seniverse.com";
+const char* apiUrl = "https://api.seniverse.com/v3/weather/now.json";
+// 要查询天气的城市名称，可按需修改
+const char* city = "xiamen";
+WiFiClient client;
 bool getWeatherInfo() {
     if (client.connect(host, 80)) {
-        Serial.println("已连接到天气服务器");
+        //Serial.println("已连接到天气服务器");
          // 构建请求URL
         String requestUrl = String(apiUrl) + "?key=" + apiKey + "&location=" + city + "&language=zh-Hans&unit=c";
         // 发送GET请求
@@ -14,7 +23,7 @@ bool getWeatherInfo() {
         client.print("Connection: close\r\n");
         client.print("\r\n");
         delay(4000);
-        Serial.printf("Initial available bytes: %d\n", client.available());
+        //Serial.printf("Initial available bytes: %d\n", client.available());
         // 接收服务器响应
         //定义answer变量用来存放请求网络服务器后返回的数据
         String answer;
@@ -25,8 +34,8 @@ bool getWeatherInfo() {
         }
         //断开服务器连接
         client.stop();
-        Serial.println("answer:"+answer);
-        Serial.println("closing connection");
+        //Serial.println("answer:"+answer);
+        //Serial.println("closing connection");
 
         //获得json格式的数据
         String jsonAnswer;
@@ -44,6 +53,7 @@ bool getWeatherInfo() {
         // Stream& input;
         StaticJsonDocument<1024> doc;
         DeserializationError error = deserializeJson(doc, jsonAnswer);
+        /*
         if (error) {
             Serial.print("deserializeJson() failed: ");
             Serial.println(error.c_str());
@@ -51,6 +61,7 @@ bool getWeatherInfo() {
 
         }
         else Serial.println("deserializeJson() success");
+        */
         // 提取天气信息
         const char* weather = doc["results"][0]["now"]["text"].as<const char*>();
         const char* location = doc["results"][0]["location"]["name"].as<const char*>();
